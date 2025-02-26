13b11e58e3218155406aa490ccdb2df46cd27528
AH-DevWorks <alext73736@gmail.com>
2025-02-26 21:08:54 +0800

    Initial

---
+++ README.md
@@ -0,0 +1,48 @@
+
+# python-mini-games
+
+## 簡介
+   + 這個倉庫(Repository)包含我在學習和實踐 Python 過程中的參考課程、資源、建立的各種基礎小型遊戲專案以及成果等。
+   + 這些專案都是基於線上課程中的教學內容進一步延伸、擴充而成，每個專案都展示了部份的 Python 概念和技術，反映我個人的學習和技能成長歷程。
+   + 此倉庫集中於遊戲類專案，其他演算法專案請見另一倉庫: [python-algorithms](https://github.com/AH-DevWorks/python-algorithms)
+
+## 課程參考
++ Udemy - [100 Days of Code: The Complete Python Pro Bootcamp](https://www.udemy.com/course/100-days-of-code/)
+  + 課程涵蓋了 Python 從基礎語法、流程控制及演算法、網頁開發，到資料科學、AI等進階主題知識。
++ Udemy - [以 SQLite 入門 SQL | Introduction to SQL with SQLite](https://www.udemy.com/course/introduction-to-sql-with-sqlite)
+  + 課程從 SQL 基本觀念、實作到測驗循序漸進，內容涵蓋建立學習環境、資料表選擇、運算符與函數的應用、排序、篩選、條件邏輯、分組與聚合、子查詢以及資料表關聯，學習在自己的電腦上建立 SQLite 環境，並掌握關聯式資料庫與 SQL 查詢語法等基礎技能。
++ Udemy - [机器学习 A-Z (Machine Learning A-Z in Chinese)](https://www.udemy.com/course/machinelearningchinese/)
+  + 學習機器學習的完整知識體系，從資料前處理、回歸、分類、聚類、關聯規則，到強化學習、自然語言處理、深度學習、降維與模型選擇等各個面向；透過 Python 和 R 的程式碼範本及案例，掌握構建並運用機器學習模型的能力
++ 線上資源 - [《Hello 演算法》](https://www.hello-algo.com/zh-hant/)
+
+## 專案列表
++ 依完成日期排序（遞減）
+> 執行方式： 1. 下載或 clone 此倉庫； 2. 進入對應的資料夾，如：`cd "子資料夾名稱"`； 3. 在該資料夾中使用命令提示字元 (CMD) 或終端機執行 Python 檔案，如： `python (檔名).py`。
+>> 另可參考各專案資料夾內執行截圖檔案。
+
+1. [Treasure of Tunes](./)
+  + **簡易尋寶遊戲——Treasure of Tunes**
+    + 玩家是個冒險者，來到 Muse Island 尋找失落的旋律（lost melody）。
+    + 玩家必須透過輸入自己的選擇，成功抵達Happy Ending。過程中只要稍有疏失，立刻就會Game Over。
+    + 可重新「接關」從失敗的地方重新開始。
+    + 防呆設計，避免無效或複數輸入（如輸入指定範圍以外的文字）
+    + 搭配簡單的ASCII Art增加視覺豐富度。
+  + **使用的主要技術/概念**
+    + 以while loop搭配conditions進行防呆設計，避免使用者不合理的輸入
+    + 搭配錯誤提示文字，讓使用者重新輸入直到合理為止
+  + 完成日期: *[2025-02-26]*
+  + <span style="color: darkorange">未來預計延伸/改進：</span>
+    + 把遊戲的不同階段包裝成不同的Funcitons，提昇易讀性且方便管理
+    + 考慮將Game Over玩家選擇重新開始遊戲時，是跳回遊戲最初階段而非階段性的起點
+    + 擴展遊戲內容，增加豐富度與吸引力
+    + 增加GUI界面；或把程式變成網頁版 WEB 應用程式
+
+2. [待持續更新]
+
+
+
+
+*首次建立：[2025-02-26]*  
+*最後更新: [2025-02-26]*
+
+
---
+++ Treasure of Tunes/TreasureOfTunes.py
@@ -0,0 +1,80 @@
+print('''
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⣘⣩⣅⣤⣤⣄⣠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠄⢈⣻⣿⣿⢷⣾⣭⣯⣯⡳⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣧⠻⠿⡻⢿⠿⡾⣽⣿⣳⣧⡷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢰⡶⢈⠐⡀⠀⠀⠁⠀⠀⠀⠈⢿⡽⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢫⢅⢠⣥⣐⡀⠀⠀⠀⠀⠀⠀⢸⢳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠆⠡⠱⠒⠖⣙⠂⠈⠵⣖⡂⠄⢸⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⠆⠀⠰⡈⢆⣑⠂⠀⠀⠀⠀⠀⠏⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢗⠀⠱⡈⢆⠙⠉⠃⠀⠀⠀⠀⠃⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠦⡡⢘⠩⠯⠒⠀⠀⠀⢀⠐⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡄⢔⡢⢡⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⢆⠸⡁⠋⠃⠁⠀⢀⢠⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡰⠌⣒⠡⠄⠀⢀⠔⠁⣸⣿⣷⣤⣀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣐⣤⡄⠀⠀⠘⢚⣒⢂⠇⣜⠒⠉⠀⢀⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣦⣔⣀⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⡀⢀⢠⣤⣶⣿⣿⣿⡆⠀⠀⠐⡂⠌⠐⠝⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⢨⣶⣿⣿⣿⣿⣿⣿⣿⣿⣤⡶⢐⡑⣊⠀⡴⢤⣀⣀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠷⡈⠀⠶⢶⣰⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⢾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣉⠑⠚⣙⡒⠒⠲⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡁⠀⠀⠀⠀⠀⠀⠀⠀
+⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡷⠶⠀⠀⠤⣬⣍⣹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀
+⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣛⣙⠀⢠⠲⠖⠶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣭⣰⢘⣙⣛⣲⣿⣿⣿⣿⡿⡻⠿⠿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀
+⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠶⢾⡠⢤⣭⣽⣿⣿⣿⣿⡟⣱⠦⠄⠤⠐⡄⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⡀⠀
+⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡛⣻⡕⠶⠶⣿⣿⣿⣿⣿⣿⣗⣎⠒⣀⠃⡐⢀⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀
+⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣭⣹⣏⣛⣛⣿⣿⣿⣿⣿⣿⣿⣞⣍⣉⢉⠰⠀⠠⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠅
+⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠶⢼⡧⢤⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣣⣡⣛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣅
+⡿⣷⣽⡿⠛⠋⠉⣉⡐⠶⣾⣾⣟⣻⡕⠶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣹⣫⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠗
+⢸⣿⣟⣥⡶⢘⡻⢶⡹⣛⣼⣿⣯⣽⢯⣙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⣿⣿⣿⣿⣿⣿⡿⠿⠟⠁⠀
+⠘⢟⣾⣿⣿⣚⠷⣳⢳⣫⣽⣿⣛⣾⡷⢾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠁⠀⠈⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠙⢋⣿⣿⣯⣙⣯⣵⣿⣿⣯⣽⣟⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠉⠛⢻⠟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣟⡟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣡⣿⣿⣿⣿⡗⣮⢻⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+''')
+
+can_continue = True
+print("Welcome to Muse Island!\nYour mission is to find the lost melody.")
+while can_continue:
+    guitar_or_drum = input("\nChoose your instrument:\n\tType 'guitar' or 'drum'\n").lower()
+    if guitar_or_drum == "guitar":
+        print("You picked up the guitar and played a moving intro, drawing the admiration of the local residents.")
+        break
+    elif guitar_or_drum == "drum":
+        print("You tried striking the drum, but completely missed the mark, angering the islanders present.\nYou have been expelled from Muse Island.\nGame Over.")
+        continue_check = input("Do you wanna try again? (Press 'Y' to continue) ").lower()
+        if continue_check != "y":
+            can_continue = False
+            break
+    else:
+        print("Oops, invalid input. Please try again!")
+
+while can_continue:
+    listen_or_improvise = input("\tA kind local has pointed you in the right direction, leading you to a mystically glowing stone wall.\nEnter 'listen' to carefully analyze the natural melodies around you, or 'improvise' to create spontaneously.\n").lower()
+    if listen_or_improvise == "listen":
+        print("You listen carefully to nature's melodies and discover a hidden path beside a cluster of bushes behind you!\nIt appears that is the true entrance!\n\nFollowing the path, you arrive at a fork in the road:")
+        break
+    elif listen_or_improvise == "improvise":
+        print("Your impromptu performance was dreadful, scaring all the nearby creatures away and leaving an eerie silence.\nYou are forced to return empty-handed.\nGame Over.")
+        continue_check = input("Do you wanna try again? (Press 'Y' to continue) ").lower()
+        if continue_check != "y":
+            can_continue = False
+            break
+    else:
+        print("Oops, invalid input. Please try again!")
+
+while can_continue:
+    left_or_right_or_back = input("To the left leads to a small lake, but beside the path lies an injured cheetah lying on its side;\nTo the right, the destination is unknown, yet a gentle melody drifts from afar...\nWhat's your choice?\n\t1: Left | 2: Right | 3: Turn back\n")
+    if left_or_right_or_back == "1":
+        print("You muster the courage to bandage the injured cheetah. But then, the cheetah suddenly speaks!")
+        print("It turns out this was a test from the God of Music!\nSince you didn't give the cheetah up, you're rewarded by Him!")
+        print("Congratulations! You find the Lost Melody ---\n\t!!![[Rickroll Rhapsody]]!!!")
+        print("\t~Happy Ending~")
+        break
+    elif left_or_right_or_back in ["2", "3"]:
+        print("Oh... a true musician would never give up on any injured creature...\nYou are suddenly teleported back home, left alone to cry into your blanket...\nGame Over.")
+        continue_check = input("Do you wanna try again? (Press 'Y' to continue) ").lower()
+        if continue_check != "y":
+            can_continue = False
+            break
+    else:
+        print("Oops, invalid input. Please try again!")
